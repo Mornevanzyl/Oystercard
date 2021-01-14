eb117fbb6fbaed8ea04ba35ee6a58ff552d9018e
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-14 16:45:11 +0000

    step14 started writing Journey test

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,11 +2,18 @@
 
 class Journey
 
+  MIN_FARE = 1
+  PENALTY_FARE = 6
+
   attr_accessor :entry_station, :exit_station
 
   def initialize(entry_station, exit_station)
     @entry_station = entry_station
     @exit_station = exit_station
+  end
+
+  def fare
+    PENALTY_FARE
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,10 +1,26 @@
 require 'journey'
+require "station"
 
 describe Journey do
+  let(:station1) { Station.new("Waterloo", "1") }
+  let(:station2) { Station.new("Euston", "1") }
 
-  subject(:journey) { described_class.new(nil, nil) }
   it { is_expected.to respond_to :entry_station }
   it { is_expected.to respond_to :exit_station }
+  it { is_expected.to respond_to :fare }
+
+  subject(:journey) { described_class.new(station1, nil) }
+  it "returns penalty fare" do
+    expect(journey.fare).to eq described_class::PENALTY_FARE
+  end
+
+  describe "completed journeys" do
+    subject(:journey) { described_class.new(station1, station2) }
+
+    it "returns minimum fare" do
+      expect(journey.fare).to eq described_class::MIN_FARE
+    end
+  end
 
 
 end
